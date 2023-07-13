2d89c83511bbbe410be02045998715ae46d3ab5d
JacobTGH <139144703+JacobTGH@users.noreply.github.com>
2023-07-13 18:19:54 +0100

    Update main.py

--- Recipe_Manager/main.py
+++ Recipe_Manager/main.py
@@ -1,1 +1,10 @@
+from recipe import Recipe
 
+cook_time = 10
+recipe_name = "egg"
+dietary_info = "egg"
+ingredient_list = ["egg", "water", "pan"]
+instruction_list = ["boil water", "put egg in water", "wait 10 minutes", "egg is cooked"]
+egg = Recipe(recipe_name, ingredient_list, instruction_list, cook_time, dietary_info)
+
+egg.get_details()
