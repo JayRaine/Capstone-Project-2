d09aa291723e15dd172ec32828d74e8a80eebc2f
Yaakov Lewin <yaakovlewin@gmail.com>
2023-07-20 04:14:34 +0100

    minor updates

--- Recipe_Manager/main.py
+++ Recipe_Manager/main.py
@@ -5,47 +5,59 @@ from recipes import recipes  # Import the 'recipes' dictionary from recipes.py
 
 recipe_manager = RecipeManager()  # Create an instance of the RecipeManager class
 
+
 # Create a Recipe instance using the 'egg_recipe' dictionary from recipes.py
 recipe_instance = Recipe(**recipes['egg_recipe'])
+
 
 # Add the created recipe to the recipe manager
 recipe_manager.add_recipe(recipe_instance)
 
 
-
-
 # Get all recipes from the recipe manager
 all_recipes = recipe_manager.get_all_recipes()
-for recipe in all_recipes:  # Iterate over each recipe
-    print(recipe.get_details(['title', 'cooking_time',
-                              'dietary_info', 'ingredients', 'instructions']))  # Print details of each recipe
+# Print all recipes
+recipe_manager.print_recipes(all_recipes, ['cooking_time',
+                                           'dietary_info', 'ingredients', 'instructions', 'equipment'])  # Print details of the first recipe in the list
 
-# Get the recipe with the title "egg" from the recipe manager (returns a list)
-egg_recipe = recipe_manager.get_recipe_by_title("egg")
-# Print the details of the egg recipe in the list
+
+# Get the recipes from the recipe manager (returns a list)
+
+# Get the recipe with the title "egg"
+egg_recipe = recipe_manager.get_recipe_by_title(
+    "egg")
+# Get all 10 minute recipes
+ten_min_recipes = recipe_manager.get_recipes_by_cooking_time(
+    10)
+
+# Print the details of the egg recipe
 recipe_manager.print_recipes(egg_recipe, ['cooking_time',
-                                 'dietary_info', 'ingredients', 'instructions']) # Print details of the first recipe in the list
+                                          'dietary_info', 'ingredients', 'instructions', 'equipment'])
+# Print all 10 minute recipes
+recipe_manager.print_recipes(ten_min_recipes, ['cooking_time',
+                                               'dietary_info', 'ingredients', 'instructions', 'equipment'])
+
 
 # update recipe functions by attributes
-recipe_manager.update_recipe_title("egg", "egg2")
 recipe_manager.update_recipe_cooking_time("egg", 20)
-recipe_manager.update_recipe_dietary_info("egg2", "egg2")
-recipe_manager.update_recipe_ingredients("egg2", ["egg2", "water2", "pan2"])
-recipe_manager.update_recipe_instructions("egg2", ["boil water2", "put egg2 in water2", "wait 20 minutes", "egg2 is cooked"])
+recipe_manager.update_recipe_dietary_info("egg", "egg is good for you")
+recipe_manager.update_recipe_ingredients("egg", ["egg", "water", "salt"])
+recipe_manager.update_recipe_instructions(
+    "egg2", ["boil water", "add egg", "add salt"])
+recipe_manager.update_recipe_equipment("egg", ["pot", "spoon", "whisker"])
+recipe_manager.update_recipe_title("egg", "scrambled egg")
 
-egg_recipe2 = recipe_manager.get_recipe_by_title("egg2")
+# get the new recipe
+egg_recipe2 = recipe_manager.get_recipe_by_title("scrambled egg")
+
+# print the new recipe
 recipe_manager.print_recipes(egg_recipe2, ['cooking_time',
-                                 'dietary_info', 'ingredients', 'instructions']) # 
-print(egg_recipe2[0].get_details(['title', 'cooking_time',
-                                'dietary_info', 'ingredients', 'instructions']))
-# patatoes_recipe = {
-#     'cooking_time': 10,
-#     'recipe_title': "patatoes",
-#     'dietary_info': "patatoes",
-#     'ingredient_list': ["patatoes", "water", "pan"],
-#     'instruction_list': ["boil water", "put patatoes in water", "wait 10 minutes", "patatoes is cooked"]
+                                           'dietary_info', 'ingredients', 'instructions', 'equipment'])
 
-# }
-# egg.get_details()
+# delete recipe
+recipe_manager.delete_recipe("scrambled egg")
 
-
+# print all recipes again showing that scrabled egg is deleted
+print("All recipes after deleting scrambled egg")
+recipe_manager.print_recipes(all_recipes, ['cooking_time',
+                                           'dietary_info', 'ingredients', 'instructions', 'equipment'])
